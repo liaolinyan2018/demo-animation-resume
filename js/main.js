645f8f8e21024dea05da105a1a204cdf70018489
liaolinyan2018 <linyan.lao@gmail.com>
2019-02-21 22:35:35 +0800

    会动的简历收工

--- js/main.js
+++ js/main.js
@@ -39,8 +39,10 @@ html{
   transform: rotate(360deg)
 }
 /*不玩了，我来介绍一下自己吧
-  我需要一张白纸
+  我需要一张纸
 */
+`
+var result2 = `
 #code{
   position: fixed;
   left: 0;
@@ -64,28 +66,25 @@ html{
   height: 100%;
   padding: 8px;
 }
-`
-var result2 = `
 
-
+/* 这样我就能在纸上写字啦，请看右边 */
 `
 var result3 = `
-/* 接下来把markdown变成HTML-marked.js */
-
-
+/* 接下来把markdown变成HTML */
+`
+var result4 = `
 /* 接下来给HTML加样式 */
-
-
+`
+var result5 = `
 /* 这就是我会动的简历了*/
 /* 谢谢观看 */
-
 `
 
 var md = 
 `# 自我介绍
 
-我叫***，19xx年x月出生
-现就读于北京**大学，大四
+我叫xxx，19xx年x月出生
+现就读于北京xx大学，大四
 自学前端半年
 希望应聘前端开发岗位
 
@@ -95,9 +94,9 @@ var md =
 
 # 项目介绍
 
-1. xxx轮播
-2. 简约画板
-3. 个人在线简历
+1.苹果风格轮播
+2.简约画板
+3.个人在线简历
 
 # 联系方式
 
@@ -106,21 +105,38 @@ var md =
 邮箱 xxx@gmail.com
 `
 
-writeCode('',result,() => { //writeCode call function back
+writeCode('',result,()=>{ //writeCode call function back
   console.log('结束了')
-  createPaper(() => {
-    console.log('paper准备好了')
+  createPaper(()=>{
+    console.log('paper造好了')
     writeCode(result,result2,()=>{
       console.log('纸的样式加好了')
-      writeMarkdown(md,()=>{console.log('内容也写好了')})
-    })
-  }); 
+      writeMarkdown(md,()=>{
+        console.log('简历内容也写好了')
+        writeCode(result + result2,result3,()=>{
+          markdownToHtml(()=>{
+            console.log('markdown已经变成HTML')
+            writeCode(result + result2 + result3,result5,()=>{
+              console.log('简历完成')
+            })
+            /*writeCode(result + result2 + result3,result4,()=>{
+              console.log('HTML样式添加好了')
+              writeCode(result + result2 + result3 + result4,result5,()=>{
+              console.log('全部结束')
+              })
+            })*/
+          })
+        })
+      })
+    }); 
+  })
 })
 
+/* 将md格式的内容加入到简历中 */
 function writeMarkdown(markdown,fn){
   let paperContent = paper.querySelector('.content')
-  var n = 0
-  var id = setInterval(() => {
+  let n = 0
+  let id = setInterval(() => {
     n+=1;
     paperContent.innerHTML = markdown.substring(0,n);
     paperContent.scrollTop = paperContent.scrollHeight  //每写一句代码，paperContent元素向上滚动最大幅度
@@ -128,15 +144,28 @@ function writeMarkdown(markdown,fn){
       window.clearInterval(id)
       fn.call()
     }
-  },30)
-
+  },20)
 }
 
+/*将markdown转为html*/
+function markdownToHtml(fn){
+  let paperContent = document.querySelector('.content')
+  let n = 0
+  let id = setInterval(() => {
+    n+=1;
+    paperContent.innerHTML = marked(md).substring(0,n);
+    paperContent.scrollTop = paperContent.scrollHeight  //每写一句代码，paperContent元素向上滚动最大幅度
+    if(n>=marked(md).length){
+      window.clearInterval(id)
+      fn.call()
+    }
+  },20)
+}
 /*把code写到#code 和 #styleTag标签 里*/
 function writeCode(prefix,code,fn){
-  var codeDom = document.querySelector('#code')
-  var n = 0
-  var id = setInterval(() => {
+  let codeDom = document.querySelector('#code')
+  let n = 0
+  let id = setInterval(() => {
     n+=1;
     codeDom.innerHTML = Prism.highlight(prefix + code.substring(0,n), Prism.languages.css, 'css');
     styleTag.innerHTML = prefix + code.substring(0,n)
